90014d6a355f8a9771aadd22abf3044f1c3526d1
akanaskhan <muhammadanaskhanak@gmail.com>
2025-01-26 15:50:39 +0500

    main

--- modules/users/controllers/registerUserController.js
+++ modules/users/controllers/registerUserController.js
@@ -3,8 +3,10 @@ import createUserService from "../services/registerUserService.js";
 const registerUserController = async (req, res) => {
   try {
     const user = await createUserService(req.body);
+    console.log("RESPONSE OF REGISTERATION =>", JSON.stringify(user));
     res.status(user.status).send(user);
   } catch (error) {
+    console.log("ERROR IN REGISTERATION =>", JSON.stringify(error));
     res.status(error.status).send(error);
   }
 };
--- router/index.js
+++ router/index.js
@@ -1,10 +1,12 @@
 import chalk from "chalk";
 import { express } from "../shared/constants/index.js";
 import loanRoutes from "../modules/loans/index.js";
+import userRoutes from "../modules/users/index.js";
 const router = express.Router();
 router.get("/", (req, res) => {
   res.send("Routes Page is Preset");
   console.log(chalk.bgYellow("Routes Page is Preset"));
 });
+router.use("/user", userRoutes);
 router.use("/loan", loanRoutes);
 export default router;
